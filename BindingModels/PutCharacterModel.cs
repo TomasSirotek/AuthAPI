0becc62b380aae9b00b78ec023f4b35c2d364346
Tomass <>
2022-06-11 19:46:31 +0200

    setting up

---
+++ BindingModels/PutCharacterModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Data_Access.BindingModels {
+    public class PutCharacterModel {
+        [Required]
+        [Key]
+        public string Id { get; set; } 
+
+        public string FullName { get; set; } 
+
+        public bool Status { get; set; } 
+
+        public string KnownFor { get; set; } 
+    
+        public string Gender { get; set; } 
+    
+        public DateTime DoB { get; set; } 
+    
+        public string Nationality { get; set; } 
+    }
+}
