f6b03135f7d82a1439fbd91e133d1e8902f7c3ad
Tomass <>
2022-06-13 15:47:32 +0200

    deleted migrations

--- Program.cs
+++ Program.cs
@@ -29,18 +29,18 @@ builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
 // builder.Services.AddScoped<DataSeeder>();
 //
  var app = builder.Build();
-// if (args.Length == 1 && args[0].ToLower() == "InitialSeed")
-//     SeedData(app);
-//
-// void SeedData(IHost app)
-// {
-//     var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-//     using (var scope = scopedFactory?.CreateScope())
-//     {
-//         var service = scope?.ServiceProvider.GetService<DataSeeder>();
-//         service?.Seed();
-//     }
-// }
+if (args.Length == 1 && args[0].ToLower() == "InitialSeed")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory?.CreateScope())
+    {
+        var service = scope?.ServiceProvider.GetService<DataSeeder>();
+        service?.Seed();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
